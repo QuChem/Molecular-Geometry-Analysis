21c52f72d676b04c53b4ea5b6f245b1bd2521700
Reza Hemmati <50717617+QuChem@users.noreply.github.com>
2020-09-25 16:37:17 -0500

    Update read_geometry.py

--- read_geometry.py
+++ read_geometry.py
@@ -1,4 +1,24 @@
+# This script calculates the repulsion energy of a molecue in Hartree.
+# Title:	read_geometry.py
+# Author:	Reza Hemmati
+# Created	09/25/2020
+#
+# This script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
 # GEOMETRY ANALYSIS
+# Usage: python3  read_geometry.py  xyz_file
 
 import numpy as np
 import math, sys
